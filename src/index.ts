018afdd4eb31548d2166b340644190815494d8ec
Craig Russell <craig.russell@cloudcall.com>
2017-10-11 08:56:02 +0100

    Ads logger middleware and demo routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { HttpError, NotFound, InternalServerError } from "http-errors";
+import { HttpError, NotFound, InternalServerError, Forbidden } from "http-errors";
 
 const HOST = "0.0.0.0";
 const PORT = 8000;
@@ -10,41 +10,43 @@ const app: express.Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Root endpoint
-app.get("/", (req, res, next) => {
-    res.send("Hello World");
+// Log request/response middleware
+app.use((req, res, next) => {
+    function requestLogger() {
+        res.removeListener("finish", requestLogger);
+        res.removeListener("close", requestLogger);
+        console.log(`${res.statusCode} ${req.method} ${req.url}`);
+    }
+    res.on("finish", requestLogger);
+    res.on("close", requestLogger);
     next();
 });
+
+// GET / - Return successful response
+app.get("/", (req, res) => { res.send("Hello World"); });
+
+// GET /error - throws standard Error, returns Internal Server Error response
+app.get("/error", (req, res) => { throw new Error("Danger Will Robinson!"); });
+
+// GET /forbidden - throws Forbidden Error, returns Forbidden response
+app.get("/forbidden", (req, res) => { throw new Forbidden("lol nope"); });
 
 // 404 Not Found
-app.use((req, res, next) => {
-    if (!res.headersSent) {
-        throw new NotFound();
-    }
-    next();
-});
+app.use((req, res, next) => { throw new NotFound(); });
 
 // Error handler
-app.use((err: HttpError, req, res, next) => {
-    if (!res.headersSent) {
-        if (err instanceof HttpError) {
-            // Response with thrown HTTP Errors
-            res.status(err.statusCode);
-            res.send(err.message);
-        } else {
-            // Log other Errors and respond with Internal Server Error
-            console.error(err);
-            const ise = new InternalServerError();
-            res.status(ise.statusCode);
-            res.send(ise.message);
-        }
+app.use((err: HttpError | Error, req, res, next) => {
+    if (err instanceof HttpError) {
+        // Response with thrown HTTP Errors
+        res.status(err.statusCode);
+        res.send(err.message);
+    } else {
+        // Log other Errors and respond with Internal Server Error
+        console.error(err);
+        const ise = new InternalServerError();
+        res.status(ise.statusCode);
+        res.send(ise.message);
     }
-    next();
-});
-
-// Request Logger
-app.use((req, res) => {
-    console.log(`${res.statusCode} ${req.method} ${req.url}`);
 });
 
 // Serve app
