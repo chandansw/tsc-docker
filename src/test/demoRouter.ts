399b61c6ed0f31c80ebf393220d0615ec61f6db7
Craig Russell <craig@craig-russell.co.uk>
2017-10-26 20:45:25 +0100

    Added tests for Demo Router

---
+++ src/test/demoRouter.ts
@@ -0,0 +1,75 @@
+import "mocha";
+import * as assert from "assert";
+import * as supertest from "supertest";
+import { Application } from "../lib/application";
+
+const app = supertest(Application.getInstance());
+
+describe("Basic API Routes", () => {
+
+    describe("GET /ping", () => {
+
+        it("should return a PONG response", done => {
+            app.get("/ping")
+                .expect(200, "PONG")
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    describe("GET /redir", () => {
+
+        it("should return an HTTP redirect", done => {
+            app.get("/redir")
+                .expect(302)
+                .expect("location", "https://google.com")
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    describe("GET /error", () => {
+
+        it("should return a Server Error", done => {
+            app.get("/error")
+                .expect("content-type", /json/)
+                .expect(500, {
+                    error: {
+                        status: 500,
+                        message: "Internal Server Error"
+                    }
+                })
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    describe("GET /teapot", () => {
+
+        it("should return an HTTP Error", done => {
+            app.get("/teapot")
+                .expect("content-type", /json/)
+                .expect(418, {
+                    error: {
+
+                        status: 418,
+                        message: "Not a Coffee Pot"
+                    }
+                })
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+
+    describe("POST /echo", () => {
+
+        it("should return request data", done => {
+            app.post("/echo?foo=bar")
+                .send({ fuz: "baz" })
+                .expect("content-type", /json/)
+                .expect(200)
+                .expect(res => assert.ok("headers" in res.body.data))
+                .expect(res => assert.deepEqual(res.body.data.body, { fuz: "baz" }))
+                .expect(res => assert.deepEqual(res.body.data.method, "POST"))
+                .expect(res => assert.deepEqual(res.body.data.path, "/echo"))
+                .expect(res => assert.deepEqual(res.body.data.query, { foo: "bar" }))
+                .end((err, res) => err ? done(err) : done());
+        });
+    });
+});
