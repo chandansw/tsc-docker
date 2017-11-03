a26bf614f98ec1852b4cf4c4e55803780ca477c0
Craig Russell <craig@craig-russell.co.uk>
2017-11-03 23:03:59 +0000

    Added some tests for Country endpoints

--- src/test/countryRouter.ts
+++ src/test/countryRouter.ts
@@ -3,10 +3,21 @@ import * as assert from "assert";
 import * as supertest from "supertest";
 import * as mongoose from "mongoose";
 import { Application } from "../lib/application";
+import { Country } from "../models/Country";
 
-const DBURL = process.env.DBURL || 'mongodb://localhost:27017/test';
+const DBURL = process.env.DBURL || 'mongodb://localhost:27017/testrunner';
 
 let app = supertest(Application.getInstance());
+
+const reId = /^[a-z0-9]{24}$/;
+const reTime = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}Z$/;
+
+const response404 = {
+    errors: [{
+        message: "Not Found",
+        status: 404,
+    }]
+};
 
 // Use native promises in Mongoose
 (<any>mongoose).Promise = global.Promise;
@@ -15,7 +26,8 @@ describe("Country API Routes", () => {
 
     before(done => {
         mongoose.connect(DBURL, { useMongoClient: true })
-            .then(() => done())
+            .then(() => mongoose.connection.db.dropDatabase())
+            .then(_ => done())
             .catch(done);
     });
 
@@ -26,38 +38,68 @@ describe("Country API Routes", () => {
 
     describe("GET /country", () => {
 
-        xit("should get all Countries", done => {
-            app.get("/country")
-                .expect(200, { data: [] })
-                .end(done);
+        it("should get all Countries", done => {
+
+            Country.insertMany([{ name: "UK" }, { name: "France" }, { name: "Germany" }])
+                .then(countries => countries.map(c => JSON.parse(JSON.stringify(c))))
+                .then(countries => {
+                    app.get("/country")
+                        .expect(200, { data: countries })
+                        .end(done);
+                })
+                .catch(done);
         });
     });
 
     describe("POST /country", () => {
 
-        xit("should 400 if required field missing", done => {
-            app.get("/country")
-                .expect(400, { errors: [] })
+        it("should 400 if required field missing", done => {
+            app.post("/country")
+                .send({})
+                .expect(400, {
+                    errors: [{
+                        field: "name",
+                        message: "Field `name` is required.",
+                        status: 400
+                    }]
+                })
                 .end(done);
         });
 
-        xit("should add a new Country", done => {
-            app.get("/country")
-                .expect(200, { data: [] })
+        it("should add a new Country", done => {
+            app.post("/country")
+                .send({ name: "Spain" })
+                .expect(200)
+                .expect(res => {
+                    let country = res.body.data;
+                    assert.deepEqual(Object.keys(country).sort(), ['createdAt', 'id', 'name', 'type', 'updatedAt', 'url']);
+                    assert.ok(country.id.match(reId));
+                    assert.ok(country.createdAt.match(reTime));
+                    assert.ok(country.updatedAt.match(reTime));
+                    assert.equal(country.type, "Country");
+                    assert.equal(country.name, "Spain");
+                    assert.equal(country.url, `/country/${country.id}`);
+                })
                 .end(done);
         });
     });
 
     describe("GET /country/:id", () => {
 
-        xit("should 404 if Country does not exist", done => {
-            app.get("/country/foo")
-                .expect(404, { errors: [] })
+        it("should 404 if Country does not exist (valid Object ID)", done => {
+            app.get("/country/000000000000000000000000")
+                .expect(404, response404)
+                .end(done);
+        });
+
+        it("should 404 if Country does not exist (invalid Object ID)", done => {
+            app.get("/country/000000000000000000000000x")
+                .expect(404, response404)
                 .end(done);
         });
 
         xit("should get a single Country", done => {
-            app.get("/country/foo")
+            app.get("/country/000000000000000000000000")
                 .expect(200, { data: [] })
                 .end(done);
         });
@@ -65,9 +107,17 @@ describe("Country API Routes", () => {
 
     describe("POST /country/:id", () => {
 
-        xit("should 404 if Country does not exist", done => {
-            app.get("/country/foo")
-                .expect(404, { errors: [] })
+        it("should 404 if Country does not exist (valid Object ID)", done => {
+            app.post("/country/000000000000000000000000")
+                .send({})
+                .expect(404, response404)
+                .end(done);
+        });
+
+        it("should 404 if Country does not exist (invalid Object ID)", done => {
+            app.post("/country/000000000000000000000000x")
+                .send({})
+                .expect(404, response404)
                 .end(done);
         });
 
@@ -78,7 +128,7 @@ describe("Country API Routes", () => {
         });
 
         xit("should update a single Country", done => {
-            app.get("/country/foo")
+            app.get("/country/000000000000000000000000")
                 .expect(200, { data: [] })
                 .end(done);
         });
@@ -86,14 +136,20 @@ describe("Country API Routes", () => {
 
     describe("DELETE /country/:id", () => {
 
-        xit("should 404 if Country does not exist", done => {
-            app.get("/country/foo")
-                .expect(404, { errors: [] })
+        it("should 404 if Country does not exist (valid Object ID)", done => {
+            app.delete("/country/000000000000000000000000")
+                .expect(404, response404)
+                .end(done);
+        });
+
+        it("should 404 if Country does not exist (invalid Object ID)", done => {
+            app.delete("/country/000000000000000000000000x")
+                .expect(404, response404)
                 .end(done);
         });
 
         xit("should delete a single Country", done => {
-            app.get("/country/foo")
+            app.get("/country/000000000000000000000000")
                 .expect(200, { data: [] })
                 .end(done);
         });
