3d66cc60d1155183461efde7bc56b101ae7cd98d
Craig Russell <craig@craig-russell.co.uk>
2017-11-03 19:40:46 +0000

    Transform Country for JSON API response

--- src/models/Country.ts
+++ src/models/Country.ts
@@ -1,7 +1,21 @@
 import * as mongoose from "mongoose";
 
-export const countrySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-});
+export const countrySchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                ret.type = "Country";
+                ret.url = `/country/${ret.id}`;
+                delete ret._id;
+                delete ret.__v;
+                return ret;
+            }
+        }
+    });
 
 export const Country = mongoose.model("Country", countrySchema);
