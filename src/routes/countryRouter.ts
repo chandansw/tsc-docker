8e9d3734e4dc8ff58c355bb5cda91c2de6b66686
Craig Russell <craig@craig-russell.co.uk>
2017-11-03 22:54:15 +0000

    Explicit catch() required to ensure next middleware is called for thrown Errors

--- src/routes/countryRouter.ts
+++ src/routes/countryRouter.ts
@@ -11,27 +11,32 @@ const router = Router();
 
 router.get("/country", (req, res, next) => {
     Country.find()
-        .then(data => { res.jsonp(ok(data)) }, next)
+        .then(data => { res.jsonp(ok(data)) })
+        .catch(next);
 });
 
 router.post("/country", (req, res, next) => {
     new Country(req.body).save()
-        .then(data => { res.jsonp(ok(data)) }, next)
+        .then(data => { res.jsonp(ok(data)) })
+        .catch(next);
 });
 
 router.get("/country/:id", (req, res, next) => {
     Country.findById(req.params.id)
-        .then(data => { res.jsonp(ok(data)) }, next)
+        .then(data => { res.jsonp(ok(data)) })
+        .catch(next);
 });
 
 router.post("/country/:id", (req, res, next) => {
     Country.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then(data => { res.jsonp(ok(data)) }, next)
+        .then(data => { res.jsonp(ok(data)) })
+        .catch(next);
 });
 
 router.delete("/country/:id", (req, res, next) => {
     Country.findByIdAndRemove(req.params.id)
-        .then(data => { res.jsonp(ok(data)) }, next)
+        .then(data => { res.jsonp(ok(data)) })
+        .catch(next);
 });
 
 export = router;
